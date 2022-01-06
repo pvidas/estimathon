cb71d2a4ca2b0b5b6b16515f75c56efe4707d22a
Paula Vidas <paulavidas@gmail.com>
2022-01-06 22:58:51 +0100

    Add logout view.

--- estimathon/urls.py
+++ estimathon/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import include, path
 from django.views.generic import TemplateView
 
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('games/', include('games.urls')),
     path('login', LoginView.as_view(next_page='games:index'), name='login'),
+    path('logout', LogoutView.as_view(next_page='index'), name='logout'),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
